38fa4650f4b6c0e5ea2a762882a8ec4c7fd9ac17
pdelteil@gmail.com <pdelteil@gmail.com>
2024-05-06 18:29:28 +0000

    api endpoint loaded from config file

--- getPrograms.sh
+++ getPrograms.sh
@@ -6,7 +6,6 @@ source ./config.ini
 #password=$apikeyTesting
 username=$usernameProduction
 password=$apikeyProduction
-api_url="https://api.hackerone.com/v1/hackers/programs"
 page_size=100
 page_number=1
 total_sum=0
@@ -16,7 +15,7 @@ echo "Handle, Name,Submission State, URL"
 url="https://hackerone.com"
 
 while true; do
-    response=$(curl -s -u "$username:$password" -H 'Accept: application/json' "$api_url?page\[size\]=$page_size&page\[number\]=$page_number" )
+    response=$(curl -s -u "$username:$password" -H 'Accept: application/json' "$apiEndpoint?page\[size\]=$page_size&page\[number\]=$page_number" )
     count=$(echo "$response"|jq '.data | length')
     #echo "$response"|jq
     # Use jq to extract the desired values
@@ -29,14 +28,11 @@ while true; do
     for i in $(seq 0 $((${#handles[@]} - 1))); do
         echo "${handles[$i]}, ${names[$i]}, ${submission_states[$i]}, $url/${handles[$i]}"
     done
-    #echo "Page: $page_number, Total objects: $count"
 
-    # Add the count to the total sum
     ((total_sum += count))
     # Check if the count is less than the page size
     if [[ "$count" -lt "$page_size" ]]; then
         break
-        #echo "next"
     else
     # Increment the page number for the next request
     ((page_number++))
